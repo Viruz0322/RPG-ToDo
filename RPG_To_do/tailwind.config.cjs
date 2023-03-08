947f10b59487512242c7ea39c9aa569d282e64bc
francisco perez <frperez032219@gmail.com>
2023-03-08 02:25:56 -0800

    redid using vite and added tailwind css

---
+++ RPG_To_do/tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        warprime: '#F5E05A',
+        warsecond: '#DB873d',
+        healprime: '#9CE845',
+        healsecond: '#43A856',
+        scholprime: '#79EAF2',
+        scholsecond: '#4B9EC4',
+      },
+      fontFamily: {
+        display: ['Roboto Mono', 'monospace']
+      },
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    }
+  },
+  plugins: [],
+}
