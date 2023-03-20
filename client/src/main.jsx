a5b8341a901dabb46676d23324b39fe222a3dbf3
Nathan Nguyen <nathannguyen1058@gmail.com>
2023-03-19 21:56:29 -0700

    changed routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import "./main.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App.jsx"; // Import the App component
+import App from "./App.jsx";
 import SideBar from './components/navBar';
 import Home from './pages/home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const rootElement = document.getElementById("root");
 createRoot(rootElement).render(
@@ -14,6 +16,8 @@ createRoot(rootElement).render(
         <main className="w-4/5 ">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </main>
       </div>
