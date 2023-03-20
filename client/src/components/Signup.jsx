0729c84fecef0493893becbb611c3f0593a49b21
Nathan Nguyen <nathannguyen1058@gmail.com>
2023-03-19 21:58:26 -0700

    changed port for login/signup

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -10,7 +10,7 @@ function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/signup', { username, password });
+      await axios.post('http://localhost:3001/api/signup', { username, password });
       navigate('/login');
     } catch (error) {
       console.error('Error signing up:', error);
