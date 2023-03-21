0829c628ef1586dcca98fba1e63292480745af76
francisco perez <frperez032219@gmail.com>
2023-03-21 11:57:56 -0700

    maybe fixed

---
+++ client/src/constants/rewards.js
@@ -0,0 +1,32 @@
+export const rewards = [
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Splurge on a outfit accessorie'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+    {
+        reward: 'Binge half a season of your favorite show'
+    },
+]
