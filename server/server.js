22f04d43e3bd0f6050a045a52b99f3624d98cdf0
francisco perez <frperez032219@gmail.com>
2023-03-21 15:45:30 -0700

    fixed maybe 6.0

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
+//maybe work please?
 
 db.once("open", () => {
   app.listen(PORT, () => {
